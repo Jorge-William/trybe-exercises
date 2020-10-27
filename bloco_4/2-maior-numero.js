e0ec3b71dbe018a0658a62f60c64883abc6e2576
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-10-26 22:24:12 -0300

    Programa que mostra qual numero é maior entre dois numeros

---
+++ bloco_4/2-maior-numero.js
@@ -0,0 +1,15 @@
+/**
+ * Faça um programa que retorne o maior de dois números. 
+ * Defina no começo do programa duas variáveis com os valores que serão comparados.
+ * 
+ */
+let a, b;
+
+a = 45;
+b = 56;
+
+if (a > b){
+    console.log(a + " é maior do que " + b)
+} else {
+    console.log(b + " é maior do que " + a)
+}
