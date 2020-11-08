90b3dbf1160f08ff444f0122252c1766baebafef
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-11-07 21:05:22 -0300

    Exercicio 7

---
+++ 4-2/exercício 7.js
@@ -0,0 +1,12 @@
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, ];
+var smaller = numbers[0];
+
+for(let i = 0; i < numbers.length; i++){
+    if (numbers[i] <= smaller) {
+        smaller = numbers[i];
+    }
+}
+
+console.log(smaller);
