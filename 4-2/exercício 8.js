6be4219c9ea79826ad6b3c5bd56f68cd9c7a8819
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-11-07 22:17:50 -0300

    Exercício 8

---
+++ 4-2/exercício 8.js
@@ -0,0 +1,8 @@
+// Utilizando for, crie uma array que vá de 1 até 25 e imprima o resultado;
+
+        var numeros = [0];
+
+        for (let index = 1; index <= 25; index++) {
+            numeros.push(index);
+            console.log(numeros[index]);
+        }
